98e7461e71823cc52d7491d7bbadc7248670327e
AviatorCoderr <124370316+AviatorCoderr@users.noreply.github.com>
2023-12-19 18:48:08 +0530

    changes done array file seperated

--- src/components/Topmost.jsx
+++ src/components/Topmost.jsx
@@ -5,7 +5,7 @@ function Topmost() {
       <div className="h-screen w-full">
         <img
           className="left-0 top-0 h-screen w-full object-cover bg-transparent"
-          src="src\images\4B8FECFF-C1AC-404C-AD2A-04AC080829A0.JPG"
+          src="src\images\bitphoto.JPG"
           alt="bit-im"
         ></img>
         <div className="absolute left-0 top-0 h-screen w-full bg-black/30"></div>
