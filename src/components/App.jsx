a6800649857462e61485675aa532f3fd1d3a109b
AviatorCoderr <124370316+AviatorCoderr@users.noreply.github.com>
2023-12-21 20:37:15 +0530

    more styles fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,14 @@ import Topmost from "./Topmost";
 import ListC from "./ListC";
 import Meetdev from "./Meet-devs";
 import Footer from "./Footer";
+import Bgdiv from "./bgdiv";
 function App() {
   return (
     <div>
       <Navbar />
       <Topmost />
       <Showcase />
+      <Bgdiv />
       <ListC />
       <Meetdev />
       <Footer />
