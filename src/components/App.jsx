7d178c523fa1cd25a8fa2778c5b75a138622e3ee
sumitt-kun <sumitkumar933434@gmail.com>
2024-01-09 21:40:32 +0530

    added add post feature for newsroom using firestore

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,13 @@ import Home from "./Home";
 import Login from "./Login";
 import SignIn from "./SignIn";
 import Dashboard from "./Dashboard";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import AdNews from "./AdNews";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 function App() {
   return (
     <Router>
@@ -11,34 +17,35 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<SignIn />} />
         <Route path="/signin" element={<SignIn />} />
-        <Route path="/dashboard" element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        } />
+        <Route path="/add" element={<AdNews />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
 }
 export default App;
 
-const ProtectedRoute = ({children}) => {
-  const user = localStorage.getItem('user');
-  if(user){
-    return children
-  } else {
-    return <Navigate to={'/login'} />
-  }
-}
-const arr = [
-  "iet@bitclubconnect", "ieee@bitclubconnect" 
-];
-const ProtectedRouteForAdmin = ({children})=> {
-  const admin = JSON.parse(localStorage.getItem('user'));
-  if(arr.find(admin.user.email)!==undefined){
+const ProtectedRoute = ({ children }) => {
+  const user = localStorage.getItem("user");
+  if (user) {
     return children;
+  } else {
+    return <Navigate to={"/login"} />;
   }
-  else{
-    return <Navigate to={'/login'} />
+};
+const arr = ["iet@bitclubconnect", "ieee@bitclubconnect"];
+const ProtectedRouteForAdmin = ({ children }) => {
+  const admin = JSON.parse(localStorage.getItem("user"));
+  if (arr.find(admin.user.email) !== undefined) {
+    return children;
+  } else {
+    return <Navigate to={"/login"} />;
   }
-}
+};
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth"
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyAlEHzwbJEsWfzw2S56BaMHJOHVBtN8W5c",
   authDomain: "clubconnect-6d118.firebaseapp.com",
@@ -7,10 +7,10 @@ const firebaseConfig = {
   storageBucket: "clubconnect-6d118.appspot.com",
   messagingSenderId: "691930125830",
   appId: "1:691930125830:web:32b8ddad7d873a6d24d587",
-  measurementId: "G-XXK69JQWH1"
+  measurementId: "G-XXK69JQWH1",
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
-export {auth, googleProvider};
+export { auth, googleProvider, firebaseConfig };
